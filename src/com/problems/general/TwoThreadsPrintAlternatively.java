f21c425fbbf9cee26cc1aa4c713dbf1664958dc8
Raja Jain <raja@bsb.in>
2015-06-02 00:02:15 +0530

    print word alternatively using two threads

---
+++ src/com/problems/general/TwoThreadsPrintAlternatively.java
@@ -0,0 +1,50 @@
+package com.problems.general;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class TwoThreadsPrintAlternatively {
+
+	public static void main(String[] args) {
+
+		Object obj = new Object();
+		Thread t1 = new Thread(new PrintRun(obj));
+		Thread t2 = new Thread(new PrintRun(obj));
+		t1.start();
+		t2.start();
+	}
+
+}
+
+class PrintRun implements Runnable {
+
+	private Object obj;
+	private AtomicBoolean bool;
+
+	public PrintRun(Object obj) {
+		this.obj = obj;
+		bool = new AtomicBoolean(false);
+	}
+
+	@Override
+	public void run() {
+
+		while (true) {
+			if (bool.get()) {
+
+				synchronized (obj) {
+					System.out.print("RAJA");
+					bool.set(false);
+
+				}
+			} else {
+				synchronized (obj) {
+					System.out.println("JAIN\n");
+					bool.set(true);
+
+				}
+			}
+
+		}
+	}
+
+}
