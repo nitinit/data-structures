f59393a64a5e5b2bdebf31c471b704f389896173
Raja Jain <raja@bsb.in>
2015-06-01 17:48:46 +0530

    geeks for geeks trees questions

---
+++ src/com/problems/geeksforgeeks/trees/CountLeafNodeOfBinaryTree.java
@@ -0,0 +1,14 @@
+package com.problems.geeksforgeeks.trees;
+
+/**
+ * 
+ * @author raja 01-Jun-2015
+ *
+ */
+public class CountLeafNodeOfBinaryTree {
+
+	public static void main(String[] args) {
+
+	}
+
+}
