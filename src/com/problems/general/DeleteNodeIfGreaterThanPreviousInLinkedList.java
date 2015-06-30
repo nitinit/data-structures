44080594eb6b68aff285c96e3a30780d2c32d988
Raja Jain <raja@bsb.in>
2015-06-30 18:39:53 +0530

    delete all nodes which are less than previous node in linked list

---
+++ src/com/problems/general/DeleteNodeIfGreaterThanPreviousInLinkedList.java
@@ -0,0 +1,51 @@
+package com.problems.general;
+
+import com.problems.geeksforgeeks.trees.*;
+import com.problems.geeksforgeeks.trees.TreeNode;
+
+/**
+ * Created by raja on 30/06/15.
+ */
+public class DeleteNodeIfGreaterThanPreviousInLinkedList {
+
+    public static int max = 0;
+    public static void main(String [] args){
+        com.problems.geeksforgeeks.trees.TreeNode ll = new TreeNode(10);
+        ll.next = new TreeNode(30);
+        ll.next.next = new TreeNode(9);
+        ll.next.next.next = new TreeNode(8);
+        ll.next.next.next.next = new TreeNode(20);
+        ll.next.next.next.next.next = new TreeNode(16);
+        ll.next.next.next.next.next.next = new TreeNode(2);
+
+
+        TreeNode result =  delete(ll);
+        while(ll!=null){
+            System.out.print(ll.value + " ");
+            ll=ll.next;
+        }
+        System.out.println();
+
+        while(result!=null){
+            System.out.print(result.value + " ");
+            result=result.next;
+        }
+    }
+
+    public static TreeNode delete(TreeNode head){
+        if(head.next==null){
+            max=head.value;
+            return new TreeNode(head.value);
+        }
+
+        TreeNode node = delete(head.next);
+        if(max<head.value){
+            TreeNode result = new TreeNode(head.value);
+            max = head.value;
+            result.next = node;
+            return result;
+        }
+        return node;
+    }
+
+}
