794c76169a9cf9938d0fdbef22544f0ab174de71
Raja Jain <raja@bsb.in>
2015-10-25 12:30:05 +0530

    added code for removing extra space

--- src/com/problems/general/RemoveExtraSpacesEfficient.java
+++ src/com/problems/general/RemoveExtraSpacesEfficient.java
@@ -7,27 +7,33 @@ public class RemoveExtraSpacesEfficient {
 
     public static void main(String[] args) {
 
-        String s = "my    name is    mr    space           . is there         anything          wrong                               34.      3 ";
+        String s = "my    name is    mr    space           . is some         thing          wrong                               ?      no ";
 
         char[] charArray = s.toCharArray();
 
+
+        //removing space in-place.
         int j = 0;
         int i = 0;
         for (; i < charArray.length && j < charArray.length; i++) {
             char cur = charArray[j];
+            // No need to remove first space, so skipping the same.
             if (cur == ' ') {
                 charArray[i] = charArray[j];
                 i++;
                 j++;
             }
+            // check j must be less than size of string
             if (j >= charArray.length) {
                 break;
             }
             cur = charArray[j];
+            // now skip all continuous space.
             while (cur == ' ' && j + 1 < charArray.length) {
                 j++;
                 cur = charArray[j];
             }
+            // now move the jth character to ith position(skipped all the space between i and j)
             charArray[i] = charArray[j];
             j++;
         }
